91087d7a72a9630c2001f1f1372361a0a2d2eef3
Claude Lu <claudelu@Claudes-MacBook-Pro.local>
2021-02-27 12:34:07 -0700

    create room feature added

--- RoomForm.py
+++ RoomForm.py
@@ -2,4 +2,4 @@ from wtforms import Form, StringField, validators
 
 class RoomForm(Form):
     name = StringField('Name', [validators.Length(min=4, max=15)])
-    room = StringField('Room', [validators.Length(min=6, max=6)])
+    room = StringField('Room', [validators.Length(min=0, max=6)])
