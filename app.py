e6388f0a1f0f39653de2dcccb7d78f35356ee928
Claude Lu <claudelu@Claudes-MacBook-Pro.local>
2021-02-27 10:56:24 -0700

    added chatRoom route

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_socketio import SocketIO, send
 from RoomForm import RoomForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mySecretKey'
 socketio = SocketIO(app)
 
+@socketio.on('text', namespace='/chat')
+def text(message):
+
+    send(message['msg'], room=room)
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = RoomForm(request.form)
     if request.method == 'POST' and form.validate():
-        return "Your form has been validated"
+        session['name'] = form.name.data
+        session['room'] = form.room.data
+        return redirect(url_for('chatRoom'))
     return render_template('index.html', form=form)
+
+@app.route('/chatRoom')
+def chatRoom():
+    name = session.get('name', '')
+    room = session.get('room', '')
+
+    if name == '' or room == '':
+        return render_template('index.html')
+    
+    return render_template('chatRoom.html', name=name, room=room)
 
 if __name__ == '__main__':
     socketio.run(app)
